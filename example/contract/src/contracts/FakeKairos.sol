0f0da0fd23aeab52f327b67b1d8ea1fade87faf9
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-25 14:16:01 -0700

    wip

--- example/contract/src/contracts/FakeKairos.sol
+++ example/contract/src/contracts/FakeKairos.sol
@@ -1,20 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract FakeKairos is IERC721Receiver, ECDSA {
-    function onERC721Received(
-        address,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external returns (bytes4) {
-        (uint amount, bytes memory signature) = abi.decode(data, (uint, bytes));
-
-
-
-        return this.onERC721Received.selector;
+contract FakeKairos is ECDSA {
+    function borrow(IERC721 implem, uint tokenId, uint amount, bytes signature) external {
+        
     }
 }
