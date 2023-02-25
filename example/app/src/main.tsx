c26e4d5bd727af07b4db0fb82c4116409a53daea
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-25 11:12:04 -0700

    connect wallet

--- example/app/src/main.tsx
+++ example/app/src/main.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { Chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
-const root: HTMLElement | undefined = document.getElementById('root') ?? undefined;
+const root: HTMLElement | undefined =
+  document.getElementById("root") ?? undefined;
 
 if (root === undefined) {
-  throw new Error('Undefined html root element');
+  throw new Error("Undefined html root element");
 }
+
+let network_config: Chain = {
+  id: 31337,
+  name: "local",
+  network: "localhost",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: { http: ["http://localhost:8545"] },
+    public: { http: ["http://localhost:8545"] },
+  },
+  testnet: false,
+};
+
+const { chains, provider } = configureChains(
+  [mainnet, network_config],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Composooor",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
 );
