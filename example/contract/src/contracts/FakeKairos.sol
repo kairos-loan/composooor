c7de3038a4fd5dcaf8b48442af122cf423f5b71b
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-25 15:03:37 -0700

    implem fake kairos

--- example/contract/src/contracts/FakeKairos.sol
+++ example/contract/src/contracts/FakeKairos.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract FakeKairos is ECDSA {
-    function borrow(IERC721 implem, uint tokenId, uint amount, bytes signature) external {
-        
+contract FakeKairos {
+    IERC20 constant WETH = IERC20(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
+
+    function borrow(IERC721 implem, uint tokenId, uint amount, bytes memory signature) external {
+        implem.safeTransferFrom(msg.sender, address(this), tokenId);
+        address lender = ECDSA.recover(keccak256(abi.encode(amount)), signature);
+        WETH.transferFrom(lender, msg.sender, amount);
     }
 }
