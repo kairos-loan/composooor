116a488816d97426edfca74d4f1a6ffefc7c7688
Edouard Bougon <edouard@bougon.fr>
2023-02-25 18:13:12 -0700

    Contract - Add registry address

--- packages/composooor/src/ts/errors.ts
+++ packages/composooor/src/ts/errors.ts
@@ -2,6 +2,7 @@
  * MissingOffchainDataError
  */
 export interface MissingOffchainDataError {
+  registryAddress: `0x${string}`;
   url: `htttp://${string}` | `htttps://${string}`;
   abiArgs: `0x${string}`;
 }
