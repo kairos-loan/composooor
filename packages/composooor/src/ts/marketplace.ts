8a12f6053647b26af9bafddcd4cac387a7945cd7
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-26 11:21:44 -0700

    type execute method

--- packages/composooor/src/ts/marketplace.ts
+++ packages/composooor/src/ts/marketplace.ts
@@ -1,29 +1,31 @@
-import { providers, Contract } from 'ethers';
-import { defaultAbiCoder, hexZeroPad } from 'ethers/lib/utils';
+import { providers, Contract, utils } from "ethers";
+
 // import post from 'axios';
 
-const scWalletAbi = ['function execute(Call[] memory calls) external'];
+const scWalletAbi = ["function execute(Call[] memory calls) external"];
 
 const provider = new providers.JsonRpcProvider(process.env.RPC_URL);
 
-export async function marketplace(scWalletAddr: string, callee: string, functionSelector: string, args: string) {
+export async function marketplace(
+  scWalletAddr: string,
+  callee: string,
+  functionSelector: string,
+  args: string
+) {
   const scWalletContract = new Contract(scWalletAddr, scWalletAbi, provider);
 
-  const functionSelectorAsBytes4 = hexZeroPad(functionSelector, 4);
-  const argsAsBytes = hexZeroPad(args, 32);
-  const abiEncoded = defaultAbiCoder.encode(
-    ['address', 'bytes4', 'bytes'],
-    [callee, functionSelectorAsBytes4, argsAsBytes],
+  const functionSelectorAsBytes4 = utils.defaultAbiCoder.encode(
+    ["bytes4"],
+    [functionSelector]
   );
 
-  const res = await scWalletContract.callStatic.execute([
-    {
-      to: callee,
-      data: abiEncoded,
-    },
-  ]);
+  const argsAsBytes = utils.defaultAbiCoder.encode(["bytes"], [args]);
 
-  console.log(res);
+  const res = await scWalletContract.callStatic.execute([
+    callee,
+    functionSelectorAsBytes4,
+    argsAsBytes,
+  ]);
 
   return res;
   //   const { apiUrl, params, registryAddress } = decodeRevertMessage(res);
@@ -38,10 +40,10 @@ const storeInRegistry = async (registryAddress: string, params: string) => {};
  */
 
 marketplace(
-  '0xD2547e4AA4f5a2b0a512BFd45C9E3360FeEa48Df',
-  '0x42Cc87749B4031c53181692c537622e5c3b7d061',
-  '0x1234',
-  '0x1234',
+  "0xD2547e4AA4f5a2b0a512BFd45C9E3360FeEa48Df",
+  "0x42Cc87749B4031c53181692c537622e5c3b7d061",
+  "0x1234",
+  "0x1234"
 )
-  .then(() => console.log('done'))
-  .catch(e => console.error(e));
+  .then(() => console.log("done"))
+  .catch((e) => console.error(e));
