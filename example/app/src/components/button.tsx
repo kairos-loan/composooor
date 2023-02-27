5ebb004d22e29f0c375337efa8efc3eb04a90567
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-27 16:00:34 -0700

    get function selector

--- example/app/src/components/button.tsx
+++ example/app/src/components/button.tsx
@@ -1,13 +1,13 @@
-import { BuyNowPayLater__factory } from "@composooor/contract";
 import { composooor } from "../../../../packages/composooor/src";
+import { BuyNowPayLater__factory } from "@composooor/contract";
 
 const ButtonPay = () => {
-  const bytecode = BuyNowPayLater__factory.getInterface(
-    BuyNowPayLater__factory.abi
-  ).getSighash("buyNowPayLater");
+  const bytecode =
+    BuyNowPayLater__factory.createInterface().encodeFunctionData(
+      "buyNowPayLater"
+    );
 
   const onClick = () => {
-    console.log("bytecode", bytecode);
     composooor(
       "0x2B2f78c5BF6D9C12Ee1225D5F374aa91204580c3",
       "0x42Cc87749B4031c53181692c537622e5c3b7d061",
