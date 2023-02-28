dfc827f42bc035bf8febc3cb3fba00fd209877fe
Edouard Bougon <edouard@bougon.fr>
2023-02-28 12:09:53 -0700

    Fix rebase

--- example/app/src/components/button.tsx
+++ example/app/src/components/button.tsx
@@ -1,14 +1,12 @@
-import { useComposooor } from "@composooor/composooor";
 import { useCallback } from 'react';
-import { BuyNowPayLater__factory, ComposooorRegister__factory } from '@composooor/contract';
-import { BuyNowPayLater } from '@composooor/contract';
+import { BuyNowPayLater__factory } from '@composooor/example-contract';
 import { BigNumber, Contract } from 'ethers';
-import { CallStructOutput } from '@composooor/contract/out/types/contracts/SmartContractWallet';
-import { defaultAbiCoder, FormatTypes } from 'ethers/lib/utils.js';
+import { CallStructOutput } from '@composooor/example-contract/out/tc/SmartContractWallet';
+import { defaultAbiCoder } from 'ethers/lib/utils.js';
 import { useContractWrite, usePrepareContractWrite } from 'wagmi';
-import { SmartContractWallet__factory } from '@composooor/contract';
-import {UseAsyncReturn, useAsync} from '../../../composooor/src/ts/utils/useAsync';
-import {AxiosRequestConfig} from 'axios';
+import { SmartContractWallet__factory } from '@composooor/example-contract';
+import { UseAsyncReturn, useAsync } from '@composooor/composooor';
+import { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
 type PrefixifyBy0x<T> = T extends string
@@ -36,15 +34,15 @@ const abiCoder = defaultAbiCoder
 const emptyBytes = '0x'
 type prefixedBy0x = `0x${string}`
 
-function extractErrorHex(error: Error){
+function extractErrorHex(error: Error) {
   let str = "0x"
-  if (error){
+  if (error) {
     if (error.message) {
-      const message = error.message 
+      const message = error.message
       let match = message.match(/error={.*}/g)
-      if(match) {
+      if (match) {
         str = match[0]
-        if (str.match(/"data":{.*}/g)){
+        if (str.match(/"data":{.*}/g)) {
           str = (str.match(/"data":{.*}/g) as Array<string>)[0]
           str = (str.match(/0x[0-9a-f]*/g) as Array<string>)[0]
         }
@@ -57,19 +55,19 @@ function extractErrorHex(error: Error){
 function getPrepValues(error: Error) {
   const MissingDataSigHash = "0xab3e92cf"
   const errorHex = extractErrorHex(error)
-  const foundSelector = errorHex.slice(0,10);
+  const foundSelector = errorHex.slice(0, 10);
   const abiEncodedErrorData = "0x".concat(errorHex.substring(10))
   if (foundSelector === MissingDataSigHash) {
     const decoded = abiCoder.decode(["address", "string", "bytes"], abiEncodedErrorData)
-    return {needsPrep: true, address: decoded[0], url: decoded[1], argsBytes: decoded[2]}
+    return { needsPrep: true, address: decoded[0], url: decoded[1], argsBytes: decoded[2] }
   } else {
-    return {needsPrep: false, error: error}
+    return { needsPrep: false, error: error }
   }
 }
 
 function useMyComposooor(contract: Contract, funcName: string, args: Array<any>) {
   const scWalletAddr = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
-  let registerAddr =   '0x0000000000000000000000000000000000000000'
+  let registerAddr = '0x0000000000000000000000000000000000000000'
   let url = "http://nothing.com"
   let argsBytes = "0x"
 
@@ -81,7 +79,7 @@ function useMyComposooor(contract: Contract, funcName: string, args: Array<any>)
     }
   ]
 
-  const {config, error} = usePrepareContractWrite({
+  const { config, error } = usePrepareContractWrite({
     address: scWalletAddr,
     abi: SmartContractWallet__factory.abi,
     functionName: 'execute',
@@ -97,9 +95,11 @@ function useMyComposooor(contract: Contract, funcName: string, args: Array<any>)
     }
   }
 
-  const {data: bytesToRegister} = useAxiosGet(url, {params: {
-    args: argsBytes
-  }})
+  const { data: bytesToRegister } = useAxiosGet(url, {
+    params: {
+      args: argsBytes
+    }
+  })
 
   const { write } = useContractWrite(config)
   return { write }
--- packages/composooor/src/index.ts
+++ packages/composooor/src/index.ts
@@ -2,3 +2,5 @@ export * from './ts/types/errors';
 export * from './ts/api/api.interface';
 export * from './ts/composooor';
 export * from './ts/types/common';
+export * from './ts/hooks/useAsync';
+export * from './ts/hooks/useAxios';
