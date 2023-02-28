534a8afcd1c05713f46854b6dab1c6b390910e98
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-27 21:30:18 -0700

    bnpl import in front

--- example/app/src/components/button.tsx
+++ example/app/src/components/button.tsx
@@ -1,6 +1,7 @@
 import { useComposooor } from "@composooor/composooor";
 import { useCallback } from 'react';
-import { ContractAddresses } from '../config/addresses/addresses';
+import { BuyNowPayLater__factory } from '@composooor/contract';
+// import { ContractAddresses } from '../config/addresses/addresses';
 
 const ButtonPay = () => {
   console.log('ButtonPay', import.meta.env.VITE_WALLET, import.meta.env.VITE_BUYNOWPAYLATER);
@@ -8,13 +9,7 @@ const ButtonPay = () => {
   useComposooor({
     scWalletAddr: import.meta.env.VITE_WALLET ?? '0x3aAde2dCD2Df6a8cAc689EE797591b2913658659' as `0x${string}`,
     address: import.meta.env.VITE_BUYNOWPAYLATER ?? '0xab16A69A5a8c12C732e0DEFF4BE56A70bb64c926' as `0x${string}`,
-    abi: [{
-      "inputs": [],
-      "name": "buyNowPayLater",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }],
+    abi: BuyNowPayLater__factory.abi,
     functionName: 'buyNowPayLater',
     args: [],
   });
