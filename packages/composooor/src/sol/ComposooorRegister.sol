0bba1f3a88b22d4fff77b6c92ef31538ec858103
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-02-28 19:49:52 -0700

    hackatonized scwallet

--- packages/composooor/src/sol/ComposooorRegister.sol
+++ packages/composooor/src/sol/ComposooorRegister.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "forge-std/Test.sol";
+
 error MissingOffchainDataError(
     address registryAddress,
     string url,
@@ -15,12 +17,11 @@ error MissingOffchainDataError(
 contract ComposooorRegister {
     // possible future improvement : support multiple data objects for the same tx
 
-    mapping(address => mapping(uint => bytes)) private data;
-    mapping(address => uint) private nonce; // number given is considered used
+    bytes private data;
 
     /// @notice record a parameter for a one-time usage
     function recordParameter(bytes calldata _data) external {
-        data[tx.origin][nonce[tx.origin] + 1] = _data;
+        data = _data;
     }
 
     /// @notice as a contract, consume a pre-computed parameter,
@@ -29,11 +30,10 @@ contract ComposooorRegister {
     function consumeParameter(
         string memory url,
         bytes memory abiArgs
-    ) internal returns (bytes memory data_) {
-        data_ = data[tx.origin][nonce[tx.origin] + 1];
-        if (data_.length == 0) {
+    ) internal returns (bytes memory) {
+        if (data.length == 0) {
             revert MissingOffchainDataError(address(this), url, abiArgs);
         }
-        nonce[tx.origin]++;
+        return data;
     }
 }
