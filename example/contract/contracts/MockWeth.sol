b494cfaa62d244e296854519f47731bbd104424f
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-03-04 17:51:35 -0700

    mint more

--- example/contract/contracts/MockWeth.sol
+++ example/contract/contracts/MockWeth.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockWeth is ERC20 {
     constructor() ERC20("Wrapped Ethereum", "WETH") {
-        _mint(msg.sender, 100 ether);
+        _mint(msg.sender, 100_000_000 ether);
     }
 
     function give(address to) public {
