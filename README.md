b6678db09208f6ecb74afad07debd58089a3b4cd
Nicolas - tobou <npasquie@users.noreply.github.com>
2023-03-04 21:11:01 -0700

    polygon deploy

--- README.md
+++ README.md
@@ -88,3 +88,4 @@ contract Example is ComposooorRegister {
 ## Deployments
 
 - [Goerli Base](https://goerli.basescan.org/address/0x43b949724b56fd72F0Ad55d65685b7bD2F05800D#code)
+- [Polygon Mainnet](https://polygonscan.com/address/0x7165305088dfc6961937fc9bf16600373a01c5ea#code)
---
+++ example/contract/contracts/DeployMatic.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "forge-std/Script.sol";
+
+import "../contracts/MockWeth.sol";
+import "../contracts/BuyNowPayLater.sol";
+import "../contracts/FlashLender.sol";
+import "../contracts/MarketPlace.sol";
+import "../contracts/MockNFT.sol";
+import "../contracts/SmartContractWallet.sol";
+
+contract Deploy is Script {
+    function run() public {
+        vm.startBroadcast();
+
+        new BuyNowPayLater(FakeKairos(address(0)), FlashLender(address(0)), MarketPlace(address(0)), MockNFT(address(0)), MockWeth(address(0)));
+    }
+}
